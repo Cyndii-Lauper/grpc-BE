8f6fc24e3a97b5fa21c2d78c76f4151c5c3705d2
Minh <elevenine00@gmail.com>
2024-10-16 11:00:37 +0700

    chore(deps): add environment and gRPC dependencies

---
+++ services/task-service/deps.ts
@@ -0,0 +1,6 @@
+// environment variables
+export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
+
+// gRPC
+export { GrpcServer } from "https://deno.land/x/grpc_basic@0.4.7/server.ts";
+export { getClient } from "https://deno.land/x/grpc_basic@0.4.7/client.ts";
